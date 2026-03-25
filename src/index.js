b8b7563b684acf30114d3d62f40345edf5a5bae3
VanyaESBD <vanya@esbd.co.za>
2026-03-25 11:44:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Sean from './Sean';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+
+const path = window.location.pathname;
+
+root.render(
+  <React.StrictMode>
+    {path === '/sean' ? <Sean /> : <App />}
+  </React.StrictMode>
+);
